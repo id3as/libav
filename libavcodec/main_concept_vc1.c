8e3a09344ac753650b7a87cb5efbb8fc4eb4a38e
Steve Strong <steve@srstrong.com>
2013-03-18 21:37:54 +0100

    WIP on main concept

--- libavcodec/main_concept_vc1.c
+++ libavcodec/main_concept_vc1.c
@@ -25,20 +25,222 @@
 #include "libavutil/pixdesc.h"
 #include "avcodec.h"
 #include "internal.h"
+
 #include "enc_vc1.h"
 #include "config_vc1.h"
 #include "enc_vc1_def.h"
+#include "mcfourcc.h"
+#include "auxinfo.h"
+
 #include <float.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+
 typedef struct VC1Context {
   AVClass        *class;
   vc1venc_tt * v_encoder;
-  
+  bufstream_tt * videobs;
+
 } VC1Context;
+
+// implementation structure
+struct impl_stream
+{
+  uint8_t *user_bfr;
+  uint32_t user_bfr_size;
+  uint32_t user_idx;
+
+  uint8_t *bfr;
+  uint32_t idx;       // read-write index
+  uint32_t bfr_size;  // allocated size
+  uint32_t chunk_size;
+};
+
+static uint32_t fw_usable_bytes(bufstream_tt *bs)
+{
+  struct impl_stream* p = bs->Buf_IO_struct;
+  return p->bfr_size - p->idx;
+}
+
+
+// request the buffer with at least numbytes-bytes
+static uint8_t *fw_request(bufstream_tt *bs, uint32_t numbytes)
+{
+  struct impl_stream* p = bs->Buf_IO_struct;
+
+  if(p->idx + numbytes <= p->bfr_size)
+    return p->bfr + p->idx;
+
+  if(p->idx)
+  {
+	if ((p->user_idx + p->idx > p->user_bfr_size) || (p->user_bfr == NULL))
+      return NULL;
+
+	memcpy(&p->user_bfr[p->user_idx], p->bfr, p->idx);
+	p->user_idx += p->idx;
+  }
+
+  p->idx = 0;
+  return p->bfr;
+}
+
+
+// confirm numbytes-bytes filled in in requested after last "request"-call
+static uint32_t fw_confirm(bufstream_tt *bs, uint32_t numbytes)
+{
+  bs->Buf_IO_struct->idx += numbytes;
+  return numbytes;
+}
+
+
+// put numbytes-bytes into bufsteam
+static uint32_t fw_copybytes(bufstream_tt *bs, uint8_t *ptr, uint32_t numbytes)
+{
+  struct impl_stream* p = bs->Buf_IO_struct;
+  uint8_t *pc;
+
+  if ((pc = bs->request(bs, numbytes)) == NULL)
+  {
+    p->idx = 0;
+    return 0;
+  }
+  memcpy(pc, ptr, numbytes);
+  return bs->confirm(bs, numbytes);
+}
+
+
+// maximum chunk-size in buffer-mode (i.e. for "request"-call)
+static uint32_t fw_chunksize(bufstream_tt *bs)
+{
+  return bs->Buf_IO_struct->chunk_size;
+}
+  
+
+static uint32_t fw_auxinfo(bufstream_tt *bs, uint32_t offs, uint32_t info_ID, void *info_ptr, uint32_t info_size)
+{
+  uint32_t *ptr;
+  struct impl_stream* p = bs->Buf_IO_struct;
+
+  switch (info_ID)
+  {
+    case BYTECOUNT_INFO:
+	  ptr = (uint32_t*)info_ptr;
+	  if (ptr && (info_size == sizeof(uint32_t)))
+	  {
+        if(p->idx)
+		{
+          if ((p->user_idx + p->idx > p->user_bfr_size) || (p->user_bfr == NULL))
+            return 0;
+
+	      memcpy(&p->user_bfr[p->user_idx], p->bfr, p->idx);
+	      p->user_idx += p->idx;
+          p->idx = 0;
+		}
+		*ptr = p->user_idx;
+	  }
+	  break;
+
+  }
+  return BS_OK;
+}
+
+
+static uint32_t fw_split(bufstream_tt *bs)
+{
+  return 0;
+}
+
+
+static void fw_done(bufstream_tt *bs, int32_t Abort)
+{
+  struct impl_stream* p = bs->Buf_IO_struct;
+
+  if (p->idx)
+  {
+    if ((p->user_idx + p->idx <= p->user_bfr_size) && (p->user_bfr != NULL))
+      memcpy(&p->user_bfr[p->user_idx], p->bfr, p->idx);
+  }
+
+  free(p->bfr);
+  free(p);
+  bs->Buf_IO_struct = NULL;
+}
+
+
+static void fw_free(bufstream_tt *bs)
+{
+  if (bs->Buf_IO_struct)
+    bs->done(bs, 0);
+
+  free(bs);
+}
+
+
+static int32_t init_mem_buf_write(bufstream_tt *bs, uint8_t *buffer, uint32_t bufsize)
+{
+  bs->Buf_IO_struct = (struct impl_stream*)malloc(sizeof(struct impl_stream));
+  if (!(bs->Buf_IO_struct))
+  {
+    return BS_ERROR;
+  }
+
+  bs->Buf_IO_struct->bfr = (uint8_t*)malloc(bufsize);
+  if (!(bs->Buf_IO_struct->bfr))
+  {
+	free(bs->Buf_IO_struct);
+    return BS_ERROR;
+  }
+
+  bs->Buf_IO_struct->user_bfr      = buffer;
+  bs->Buf_IO_struct->user_bfr_size = bufsize;
+  bs->Buf_IO_struct->user_idx      = 0;
+
+  bs->Buf_IO_struct->bfr_size      = bufsize;
+  bs->Buf_IO_struct->chunk_size    = bufsize;
+  bs->Buf_IO_struct->idx           = 0;
+
+  bs->usable_bytes = fw_usable_bytes;
+  bs->request      = fw_request;
+  bs->confirm      = fw_confirm;
+  bs->copybytes    = fw_copybytes;
+  bs->split        = fw_split;
+  bs->chunksize    = fw_chunksize;
+  bs->free         = fw_free;
+  bs->auxinfo      = fw_auxinfo;
+  bs->done         = fw_done;
+  bs->drive_ptr    = NULL;
+  bs->drive        = NULL;
+
+  bs->state        = NULL;
+  bs->flags        = BS_FLAGS_DST;
+  return BS_OK;
+}
+
+
+static bufstream_tt *open_mem_buf_write(uint8_t *buffer, uint32_t bufsize)
+{
+  bufstream_tt *p;
+  p = (bufstream_tt*)malloc(sizeof(bufstream_tt));
+  if (p)
+  {
+    if (init_mem_buf_write(p, buffer, bufsize) != BS_OK)
+    {
+      free(p);
+      p = NULL;
+    }
+  }
+  return p;
+}
+
+
+static void close_mem_buf(bufstream_tt* bs, int32_t Abort)
+{
+  bs->done(bs, Abort);
+  bs->free(bs);
+}
 
 static void info_printf(const char * fmt, ...)
 {
@@ -100,7 +302,7 @@ return 0;
 
 
 // resource functions dispatcher
-void * MC_EXPORT_API get_rc(const char* name)
+static void * MC_EXPORT_API get_rc(const char* name)
 {
     if (!strcmp(name, "err_printf"))
         return (void*) error_printf;
@@ -147,13 +349,27 @@ static int VC1_frame(AVCodecContext *ctx, AVPacket *pkt, const AVFrame *frame,
 {
   VC1Context *context = ctx->priv_data;
   int ret;
-  
-  /*
-  if (vc1OutVideoPutFrame(v_encoder, input_video_buffer + img_start, line_size, width, height, fourcc, option_flags, ext_info))
+
+  void * ext_info_stack[16] = {0};
+  unsigned int option_flags = 0;
+  void ** ext_info = &ext_info_stack[0];
+
+  int plane1_size = frame->linesize[0] * ctx->height;
+  int plane2_size = frame->linesize[1] * ctx->height / 2;
+  int plane3_size = frame->linesize[2] * ctx->height / 2;
+  int s = plane1_size + plane2_size + plane3_size;
+  uint8_t *b = malloc(s);
+
+  memcpy(b, frame->data[0], plane1_size);
+  memcpy(b + plane1_size, frame->data[1], plane2_size);
+  memcpy(b + plane1_size + plane2_size, frame->data[2], plane3_size);
+
+  if (vc1OutVideoPutFrame(context->v_encoder, b, frame->linesize[0], ctx->width, ctx->height, FOURCC_I420, option_flags, ext_info) == VC1ERROR_FAILED)
     {
-      break;
+      printf("It failed\n");
+      exit(1);
     }
-  */  
+
   *got_packet = ret;
   return 0;
 }
@@ -171,10 +387,15 @@ static av_cold int VC1_init(AVCodecContext *avctx)
   
   struct vc1_param_set param_set;
   struct vc1_v_settings * v_settings = &param_set.params;
+  
+  int init_options = 0;
+  void * opt_list[10];
 
   // TODO - get from context
   double frame_rate = 25;
   int interlaced = 1;
+  int bufsize = 1000000;
+  uint8_t * buffer = malloc(bufsize);
 
   int video_type = get_video_type(avctx->width, avctx->height, frame_rate);
 
@@ -192,6 +413,12 @@ static av_cold int VC1_init(AVCodecContext *avctx)
   
   context->v_encoder = vc1OutVideoNew(get_rc, v_settings, 0, 0xFFFFFFFF, 0, 0);
 
+  context->videobs = open_mem_buf_write(buffer, bufsize);
+  
+  if(vc1OutVideoInit(context->v_encoder, context->videobs, init_options, &opt_list[0]))
+    {
+      printf("vc1OutVideoInit fails.\n");
+    }      
 
   return 0;
 }
@@ -227,5 +454,7 @@ AVCodec ff_mc_vc1_encoder = {
     .long_name        = NULL_IF_CONFIG_SMALL("Main Concept VC1"),
     .priv_class       = &class,
     .defaults         = VC1_defaults,
-    .init_static_data = VC1_init_static,
+    .init_static_data = VC1_init_static,    
+    .pix_fmts     = (const enum AVPixelFormat[]) { AV_PIX_FMT_YUV420P, AV_PIX_FMT_NONE}
+
 };
